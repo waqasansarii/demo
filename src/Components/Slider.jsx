a371a4e1cc43e71c332a5e8114536c7db73303ec
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-08-20 22:26:03 +0500

    complete slider

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,72 +1,95 @@
 import React from "react";
 // import Swiper JS
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination ,Autoplay} from "swiper/core";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper/core";
 // import  styles
 import "swiper/swiper-bundle.css";
 import "./style.css";
 // assets
 import img1 from "../Assets/std.jpg";
-import hat from "../Assets/hat.png";
-import house from "../Assets/house.svg";
+import hat from "../Assets/Brand icon.png";
+import house from "../Assets/Brand icon (2).png";
+import vector from "../Assets/Vector.png";
+// import house from "../Assets/house.svg";
+import dp1 from "../Assets/Image.png";
+import dp2 from "../Assets/Image (2).png";
+import dp3 from "../Assets/Image (3).png";
 
-SwiperCore.use([Navigation, Pagination,Autoplay]);
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
+const sliderData = [
+  {
+    profile: dp1,
+    desc: "Best Custom Apperal Website on the Market",
+    name: "Calaway Scholes",
+    uni: "University of Central Missouri Lambda Chi Alpha",
+  },
+  {
+    profile: dp2,
+    desc: "Best Custom Apperal Website on the Market",
+    name: "Kelly Itatani",
+    uni: "Chapman University",
+  },
+  {
+    profile: dp3,
+    desc: "Best Custom Apperal Website on the Market",
+    name: "Caitlyn Lux",
+    uni: "The Ohio State University Alpha Gamma Delta",
+  },
+];
 const Slider = () => {
   return (
-    <div className='slider_container'>
+    <div className="slider_container">
       <Swiper
         spaceBetween={50}
         slidesPerView={3}
         // centeredSlides={true}
         loop={true}
-        // autoplay={{ delay: 2500, disableOnInteraction: false }}
+        // autoplay={{ delay: 5000, disableOnInteraction: false }}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
-        speed={5000}
+        speed={3000}
         breakpoints={{
-            "600": {
-              "slidesPerView": 1,
-            //   "spaceBetween": 'auto',
-              'centeredSlides':true,
-              'centeredSlidesBounds':true
-            },
-            "768": {
-              "slidesPerView": 3,
-              "spaceBetween": 40,
-            },
-            "1024": {
-              "slidesPerView": 3,
-              "spaceBetween": 50
-            }
-          }}
+          600: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+            centeredSlides: true,
+            centeredSlidesBounds: true,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
       >
-        {[1, 2, 3].map(() => (
+        {sliderData.map((val) => (
           <SwiperSlide>
             <div className="cards_sliders">
               <div className="cardHead">
-                <img src={img1} alt="..." />
+                <img src={val.profile} alt="..." />
                 <div className="headTitl">
                   <p className="quotes"></p>
-                  <p className='std_info'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
-                    praesentium sed ducimus voluptates omnis est minima
-                    possimus veniam
+                  <p className="std_info">
+                    {val.desc}
                   </p>
-                  <p className="name">Unknown</p>
-                  <p className="uni">MIT University</p>
+                  <p className="name">{val.name}</p>
+                  <p className="uni">{val.uni}</p>
                 </div>
               </div>
               <div className="cardMOreInfo">
                 <h2>We do more than Greek</h2>
-                <p className='moreInfo_para'>
+                <p className="moreInfo_para">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
                   veritatis deleniti quas accusamus! Dolorem minus, dicta magnam
                   doloribus provident possimus tempora tenetur.
                 </p>
                 <div className="educationInfo">
                   <div className="infoEdt1">
-                    <img className="edt_img" src={hat} alt="..." />
+                    <img className="edt_img3" src={vector} alt="..." />
                     <h6>College Orgs & Department</h6>
                   </div>
                   <div className="infoEdt">
@@ -78,7 +101,7 @@ const Slider = () => {
                     <h6>Businesses & More</h6>
                   </div>
                 </div>
-                <button className='cusBlck_btn'>Get Started</button>
+                <button className="cusBlck_btn">Get Started</button>
               </div>
             </div>
           </SwiperSlide>
