6159e4c49625c22e5d9936049e79e6f14761851e
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-08-20 18:15:44 +0500

    updated

--- src/Components/SliderTwo.jsx
+++ src/Components/SliderTwo.jsx
@@ -6,70 +6,95 @@ import "./style.css";
 import img1 from "../Assets/std.jpg";
 import hat from "../Assets/hat.png";
 import house from "../Assets/house.svg";
-export const AutoPlay=()=> {
-    const settings = {
-      dots: false,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 2000,
-      cssEase: "linear",
-      arrows:false,
-      className: "center",
-      centerMode: true,
-      centerPadding: "0px",
-    }
-    return (
-      <div>
-        <h2>Auto Play</h2>
-        <Slider {...settings}>
-            {/* <div className="container_slider"> */}
+export const AutoPlay = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    //   autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    cssEase: "linear",
+    arrows: false,
+    className: "center",
+    centerMode: true,
+    centerPadding: "0px",
+    // responsive: [
+    //     {
+    //       breakpoint: 1024,
+    //       settings: {
+    //         slidesToShow: 3,
+    //         slidesToScroll: 1,
+    //         infinite: true,
+    //         dots: true
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 600,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 2,
+    //         initialSlide: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 480,
+    //       settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //       }
+    //     }
+    //   ]
+  };
+  return (
+    <div>
+      <h2>Auto Play</h2>
+      <Slider {...settings}>
+        {/* <div className="container_slider"> */}
 
-            {[1,2,3,4].map(()=>(
-                
-                <div className="cards_sliders">
-              <div className="cardHead">
-                <img src={img1} alt="..." />
-                <div className="headTitl">
-                  <p className="quotes"></p>
-                  <p className='std_info'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
-                    praesentium sed ducimus voluptates omnis est minima
-                    possimus veniam
-                  </p>
-                  <p className="name">Unknown</p>
-                  <p className="uni">MIT University</p>
-                </div>
-              </div>
-              <div className="cardMOreInfo">
-                <h2>We do more than Greek</h2>
-                <p className='moreInfo_para'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                  veritatis deleniti quas accusamus! Dolorem minus, dicta magnam
-                  doloribus provident possimus tempora tenetur.
+        {[1, 2, 3, 4].map(() => (
+          <div className="cards_sliders">
+            <div className="cardHead">
+              <img src={img1} alt="..." />
+              <div className="headTitl">
+                <p className="quotes"></p>
+                <p className="std_info">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+                  praesentium sed ducimus voluptates omnis est minima possimus
+                  veniam
                 </p>
-                <div className="educationInfo">
-                  <div className="infoEdt1">
-                    <img className="edt_img" src={hat} alt="..." />
-                    <h6>College Orgs & Department</h6>
-                  </div>
-                  <div className="infoEdt">
-                    <img className="edt_img" src={hat} alt="..." />
-                    <h6>High Schools</h6>
-                  </div>
-                  <div className="infoEdt">
-                    <img className="edt_img" src={house} alt="..." />
-                    <h6>Businesses & More</h6>
-                  </div>
-                </div>
-                <button className='cusBlck_btn'>Get Started</button>
+                <p className="name">Unknown</p>
+                <p className="uni">MIT University</p>
               </div>
             </div>
-            ))}
-                    {/* </div> */}
-        </Slider>
-      </div>
-    );
-}
+            <div className="cardMOreInfo">
+              <h2>We do more than Greek</h2>
+              <p className="moreInfo_para">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                veritatis deleniti quas accusamus! Dolorem minus, dicta magnam
+                doloribus provident possimus tempora tenetur.
+              </p>
+              <div className="educationInfo">
+                <div className="infoEdt1">
+                  <img className="edt_img" src={hat} alt="..." />
+                  <h6>College Orgs & Department</h6>
+                </div>
+                <div className="infoEdt">
+                  <img className="edt_img" src={hat} alt="..." />
+                  <h6>High Schools</h6>
+                </div>
+                <div className="infoEdt">
+                  <img className="edt_img" src={house} alt="..." />
+                  <h6>Businesses & More</h6>
+                </div>
+              </div>
+              <button className="cusBlck_btn">Get Started</button>
+            </div>
+          </div>
+        ))}
+        {/* </div> */}
+      </Slider>
+    </div>
+  );
+};
