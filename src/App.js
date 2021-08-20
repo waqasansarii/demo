07a7ae560b5db5523068610ead427a9bf6726113
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-08-20 21:32:17 +0500

    swiper slider update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Slider />
-      <AutoPlay />
+      {/* <AutoPlay /> */}
     </div>
   );
 }
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -20,10 +20,26 @@ const Slider = () => {
         slidesPerView={3}
         // centeredSlides={true}
         loop={true}
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        // autoplay={{ delay: 2500, disableOnInteraction: false }}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
-        speed={3000}
+        speed={5000}
+        breakpoints={{
+            "600": {
+              "slidesPerView": 1,
+            //   "spaceBetween": 'auto',
+              'centeredSlides':true,
+              'centeredSlidesBounds':true
+            },
+            "768": {
+              "slidesPerView": 3,
+              "spaceBetween": 40,
+            },
+            "1024": {
+              "slidesPerView": 3,
+              "spaceBetween": 50
+            }
+          }}
       >
         {[1, 2, 3].map(() => (
           <SwiperSlide>
